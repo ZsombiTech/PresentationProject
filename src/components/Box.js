2cf41793d67d2d74e6423081902e851a936eb14a
ZsombiTech <horvathzsombor2005@gmail.com>
2021-09-14 21:01:06 +0200

    first-commit

---
+++ src/components/Box.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Box.css";
+
+function Box() {
+  return (
+    <div>
+      <div className="all-boxes">
+        <div className="box-first">
+          <p className="box-first-text">Ide kell irni</p>
+          <div className="box-first-item"></div>
+        </div>
+        <div className="box-second">
+          <p className="box-second-text">Ide jon a cumo</p>
+          <div className="box-second-item"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Box;
---
+++ src/components/Boxes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Boxes.css";
+import Box from "./Box";
+
+function Boxes() {
+  return (
+    <div>
+      <Box />
+    </div>
+  );
+}
+
+export default Boxes;
