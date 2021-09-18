fc4ffc2ead1d8f8b68cf1f15b2cf5b578bf3c784
ZsombiTech <horvathzsombor2005@gmail.com>
2021-09-18 13:44:44 +0200

    ...

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Box.css";
 
-const number = 1;
-
-const circle = (
-  <div className="dots">
-    <div className="circle"></div>
-    <div className="circle-text">P{number}</div>
-  </div>
-);
-
 function Box() {
+  const [xertek, setXertek] = useState();
+  const [yertek, setYertek] = useState();
+
+  const number = 1;
+
+  const circle = (
+    <div className="dots">
+      <div className="circle"></div>
+      <div className="circle-text">P{number}</div>
+    </div>
+  );
+
+  const kattintas = (event) => {
+    if (328 > event.clientY > 76) {
+      setYertek(event.clientY);
+      setXertek(event.clientX);
+    }
+  };
+  const letenni = () => {};
+
   return (
-    <div className="small-box">
+    <div className="small-box" onClick={kattintas}>
       <div className="text-small-box">MARK THE COURIES with a CLICK</div>
       <div className="drawing-area"></div>
       <div className="button-done">
