d8c2e38a82d8f1fac26cea2593aeda45a1bffa44
ZsombiTech <horvathzsombor2005@gmail.com>
2021-09-18 17:13:03 +0200

    ...

--- src/components/Box.js
+++ src/components/Box.js
@@ -4,28 +4,38 @@ import "./Box.css";
 function Box() {
   const [xertek, setXertek] = useState();
   const [yertek, setYertek] = useState();
-
-  const number = 1;
+  const [itemek, setItemek] = useState([]);
 
   const circle = (
-    <div className="dots">
+    <div className="dots" style={{ top: yertek + "px", left: xertek + "px" }}>
       <div className="circle"></div>
-      <div className="circle-text">P{number}</div>
+      <div className="circle-text">P1</div>
     </div>
   );
 
   const kattintas = (event) => {
-    if (328 > event.clientY > 76) {
-      setYertek(event.clientY);
-      setXertek(event.clientX);
-    }
+    setYertek(event.clientY);
+    setXertek(event.clientX);
+    console.log(xertek, yertek);
+    const item2 = {
+      main: circle,
+      xkoord: xertek,
+      ykoord: yertek,
+    };
   };
-  const letenni = () => {};
 
   return (
     <div className="small-box" onClick={kattintas}>
       <div className="text-small-box">MARK THE COURIES with a CLICK</div>
-      <div className="drawing-area"></div>
+      <div className="drawing-area">
+        {/*itemek.map((item) => {
+          const mystyle = {
+            top: item.ykoord,
+            left: item.xkoord,
+          };
+          <div style={mystyle}>{item.main}</div>;
+        })*/}
+      </div>
       <div className="button-done">
         <p>DONE</p>
       </div>
